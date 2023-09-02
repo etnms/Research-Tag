25ddbd1841c16c7d2ef88a8d132a487b938b7e4f
etnms <etiennenemes@gmail.com>
2023-09-02 10:17:35 -0400

    added more basic functions to open/save documents

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ChangeEvent, useEffect, useState } from "react";
 import "./App.css";
 import DisplayFile from "./components/DisplayFile";
 import { BaseDirectory, createDir, exists, readTextFile, writeFile } from "@tauri-apps/api/fs";
-import { resourceDir } from '@tauri-apps/api/path';
+import { dirname, documentDir } from '@tauri-apps/api/path';
 import CreateTag from "./components/CreateTag";
 import TagList from "./components/TagList";
 import { LinesObject } from "./types/LinesObject";
@@ -15,37 +15,10 @@ function App() {
   const [linesObject, setLinesObject] = useState<LinesObject[]>([]);
 
   const [tagList, setTagList] = useState<string[]>([]);
-  const [firstAppOpen, setFirstAppOpen] = useState<Boolean>(true);
-
-  const [lastFile, setLastFile] = useState<string>("");
-  const [lastDataFile, setLastDataFile] = useState<string>("");
-
-  useEffect(() => {
-    if (firstAppOpen) {
-      setLastDataFile(localStorage.getItem("lastDataFile")!);
-      // setLastFile(localStorage.getItem("lastFile")!);
-      const file = localStorage.getItem("lastFile");
-      setFirstAppOpen(false);
-
-      readFile(file!);
-      //setFile(lastFile)
-    }
-    //localStorage.setItem("lastDataFile", lastDataFile);
-    //localStorage.setItem("lastFile", lastFile!);
-
-  }, [])
 
   const readFile = async (path: string | null) => {
     if (path !== null) {
-      const contents = await readTextFile(path!);
-      console.log(contents);
-    }
-  }
-
-  const openPreviousFile = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files && event.target.files.length > 0) {
-      //setFile(event.target.files[0]);
-      setLastFile(event.target.files[0].name);
+      await readTextFile(path!);
     }
   }
 
@@ -56,36 +29,43 @@ function App() {
     });
   };
 
-  // check if path exists
-  // await exists('avatar.png', { dir: BaseDirectory.AppData });
-
   const createDataFile = async (lineObject: LinesObject[]) => {
     try {
       const fileExist: boolean = await exists(filepath!);
-      console.log(fileExist);
       if (!fileExist) {
-        await writeFile(
-          {
-            contents: `${JSON.stringify(lineObject, null, 2)}`,
-            path: `TaggerAppData/data/${fileName}.json`,
-          },
-          {
-            dir: BaseDirectory.Document,
-          },
-        );
+        writeJSONFile(lineObject);
       }
       else {
-        const confirmed = await confirm('Are you sure?', 'Tauri');
-        const confirmed2 = await confirm('This action cannot be reverted. Are you sure?', { title: 'Tauri', type: 'warning' });
+        const confirmed = await confirm('This file already exists, do you want to replace it?', { title: 'File already exists', type: 'warning' });
+        if (confirmed) writeJSONFile(lineObject);
       }
     } catch (e) {
       console.log(e);
     }
-
   };
 
+  const updateJSONFile = async (lineObject: LinesObject[]) => {
+    try {
+      writeJSONFile(lineObject);
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
+  const writeJSONFile = async (lineObject: LinesObject[]) => {
+    const dir = await dirname(filepath!);
+    await writeFile(
+      {
+        contents: `${JSON.stringify(lineObject, null, 2)}`,
+        path: `${dir}/${fileName}.json`,
+      },
+
+    );
+  }
+
   const saveJSON = () => {
-    createDataFile(linesObject);
+    updateJSONFile(linesObject);
   }
 
   const createNewFile = async () => {
@@ -93,7 +73,7 @@ function App() {
       const newFilepath = await open({
         filters: [{
           name: 'File',
-          extensions: ['txt', 'csv']
+          extensions: ['txt'] // need to add .csv
         }]
       }) as string;
       if (newFilepath !== undefined) {
@@ -107,9 +87,7 @@ function App() {
           tags: []
         }));
         setLinesObject(newLinesObject);
-        const fileNameSplit: string | undefined = newFilepath.split("\\").pop();
-        const fileNameClear: string | undefined = fileNameSplit?.split('.')[0];
-        setFileName(fileNameClear)
+        setFileName(getFileName(newFilepath))
       }
     }
     catch (err) {
@@ -118,14 +96,41 @@ function App() {
 
   }
 
+  const getFileName: (filepath: string) => string | undefined = (filepath: string) => {
+    const fileNameSplit: string | undefined = filepath.split("\\").pop();
+    const fileNameClear: string | undefined = fileNameSplit?.split('.')[0];
+    return fileNameClear;
+  }
+
+  const openJSONFile = async () => {
+    try {
+      const documentPath = await documentDir();
+
+      const jsonfilepath = await open({
+        filters: [{
+          name: 'Data file',
+          extensions: ['json']
+        }],
+        defaultPath: `${documentPath}/TaggerAppData/data`,
+      }) as string;
+      const content: LinesObject[] = JSON.parse(await readTextFile(jsonfilepath!));
+      setFilePath(jsonfilepath);
+      setLinesObject(content);
+      setFileName(getFileName(jsonfilepath))
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+
+
   return (
     <div className="container">
-      <button onClick={() => createNewFile()}>Open file</button>
+      <button onClick={() => createNewFile()}>New file</button>
+      <button onClick={() => openJSONFile()}>Open JSON file</button>
       <p>{fileName}</p>
-      <button onClick={() => readFile(lastFile)}>Check last path</button>
       <CreateTag tagList={tagList} setTaglist={setTagList} />
       <TagList tagList={tagList} />
-      <button onClick={() => createDataFolder()}>Write data folder</button>
       <button onClick={() => createDataFile(linesObject)}>createDataFile</button>
       <DisplayFile tagList={tagList} linesObject={linesObject} setLinesObject={setLinesObject} saveJSON={saveJSON} />
     </div>
--- src/components/CreateTag.tsx
+++ src/components/CreateTag.tsx
@@ -1,21 +1,56 @@
 import React from 'react';
+import { readTextFile, writeFile } from '@tauri-apps/api/fs';
+import { open, save } from '@tauri-apps/api/dialog';
+import { documentDir, dirname } from '@tauri-apps/api/path';
 
 interface CreateTagProps {
     tagList: string[],
     setTaglist: Function,
 }
-const CreateTag: React.FC<CreateTagProps> = ({tagList, setTaglist}) => {
+const CreateTag: React.FC<CreateTagProps> = ({ tagList, setTaglist }) => {
 
     const addTag = () => {
         const newTag: string = (document.querySelector('input[name="tag-input"]') as HTMLInputElement).value;
         setTaglist([...tagList, newTag])
     }
 
+    const openTagList = async () => {
+        const documentPath = await documentDir();
+        const jsonfilepath = await open({
+            filters: [{
+                name: 'Data file',
+                extensions: ['taglist']
+            }],
+            defaultPath: `${documentPath}/TaggerAppData/data`,
+        }) as string;
+
+        const content: string[] = JSON.parse(await readTextFile(jsonfilepath!));
+        setTaglist(content);
+    }
+
+    const saveTagList = async (taglist: string[]) => {
+        const filePath: string | null = await save({
+            filters: [{
+                name: 'Tag list',
+                extensions: ['taglist']
+            }]
+        });
+        await writeFile(
+          {
+            contents: `${JSON.stringify(taglist, null, 2)}`,
+            path: filePath!,
+          },
+    
+        );
+    }
+
     return (
         <div>
+            <button onClick={() => openTagList()}>Open tag list</button>
             <label htmlFor='tag-input'>Create a new tag:</label>
             <input name='tag-input'></input>
             <button onClick={() => addTag()}>Create tag</button>
+            <button onClick={() => saveTagList(tagList)}>Save tag list</button>
         </div>
     );
 };
